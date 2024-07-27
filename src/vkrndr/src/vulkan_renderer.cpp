ae7688797c39c4c4d617ad22c3a7ea8cdebb09a3
Jan Kelemen <jkeleme@gmail.com>
2024-07-27 19:53:47 +0200

    Fix flags for loaded texture images

--- src/vkrndr/src/vulkan_renderer.cpp
+++ src/vkrndr/src/vulkan_renderer.cpp
@@ -284,9 +284,8 @@ vkrndr::vulkan_image vkrndr::vulkan_renderer::transfer_image(
         1,
         VK_SAMPLE_COUNT_1_BIT,
         format,
-        VK_IMAGE_TILING_LINEAR,
-        VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT |
-            VK_IMAGE_USAGE_SAMPLED_BIT,
+        VK_IMAGE_TILING_OPTIMAL,
+        VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
         VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
         VK_IMAGE_ASPECT_COLOR_BIT)};
 
